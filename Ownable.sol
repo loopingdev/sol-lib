7fd0ee08214d9a074dcc0c8b4e6ff98e6862f9de
loopingdev <edwinvarenoyp@gmail.com>
2022-08-09 21:47:05 +0700

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../main/Context.sol";
+import "../main/utils/Context.sol";
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
